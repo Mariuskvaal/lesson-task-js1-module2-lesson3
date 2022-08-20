465f93b94369f8b9d75ab84ab994f20722973dad
Mariuskvaal <mariuskvaal1@gmail.com>
2022-08-20 20:56:55 +0200

    jm

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Question 1 
+
+const btn = document.querySelector(".btn");
+
+function functionA() {
+    console.log("im a button");
+}
+
+btn.addEventListener ("click", functionA);
+
+// question 2
+
+const btn2 = document.querySelector(".button2")  
+
+function functionB() {
+    console.log("right button activatet");
+}
